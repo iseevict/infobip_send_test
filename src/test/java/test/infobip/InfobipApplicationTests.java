caf3ebbabc494898ddf7a49e2be55cc50e8ead5a
isee <stpark0509@gmail.com>
2025-01-20 13:06:37 +0900

    first commit

---
+++ src/test/java/test/infobip/InfobipApplicationTests.java
@@ -0,0 +1,13 @@
+package test.infobip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfobipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
